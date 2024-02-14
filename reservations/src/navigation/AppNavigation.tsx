31588a080b04780f7a011837d0a234eb79a27034
lisizejnullahu <lisi.zejnullahu@hotmail.com>
2024-02-14 03:04:00 +0100

    working correctly

--- reservations/src/navigation/AppNavigation.tsx
+++ reservations/src/navigation/AppNavigation.tsx
@@ -5,9 +5,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen, DetailsScreen} from '../screens';
 
+interface AppNavigatorProps {
+  toggleDarkMode: () => void;
+  isDarkMode: boolean;
+}
+
 const Stack = createStackNavigator();
 
-const AppNavigator = ({toggleDarkMode, isDarkMode}) => {
+const AppNavigator: React.FC<AppNavigatorProps> = ({
+  toggleDarkMode,
+  isDarkMode,
+}) => {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator
